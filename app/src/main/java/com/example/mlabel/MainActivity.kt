c8c690980d5f97e0b926f192610574bc08b8a111
Zivaik <zivai@wixels.com>
2021-10-06 12:58:32 +0200

    MLabel IDV300 assessment

---
+++ app/src/main/java/com/example/mlabel/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.mlabel
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    private val REQUEST_CODE = 100
+    private lateinit var image: InputImage
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+        val uploadBtn = findViewById<Button>(R.id.btnUpload)
+
+        uploadBtn.setOnClickListener {
+            openGalleryForImage()
+        }
+
+
+    }
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+            imgUpload.setImageURI(data?.data) // handle chosen image
+
+            try {
+                image = InputImage.fromFilePath(this, data!!.data)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+            labeler.process(image)
+                .addOnSuccessListener { labels ->
+                    for (label in labels) {
+                        val text = label.text
+                        val confidence = label.confidence
+
+                        textView.append("\nImage Text: " + text + "\nConfidence: " + confidence)
+                    }
+                }
+                .addOnFailureListener { e ->
+                    // Task failed with an exception
+                    // ...
+                }
+        }
+    }
+}
